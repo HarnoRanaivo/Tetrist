6507084bc04b943b9f8c52ceef000c70f288c84c
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-12-05 19:39:44 +0100

    corrections, ajouts

--- Marvin/IA/Eval.java
+++ Marvin/IA/Eval.java
@@ -8,19 +8,32 @@ import Component.Piece;
 
 public class Eval
 {
-    public int eval_possibilities(Grid grid, Point[][][] falls)
+    public static int[] eval_possibilities(Grid grid, Point[][][] falls)
     {
-        GridIA grid_buffer = new GridIA(grid);
-        grid_buffer.copy_grid(grid);
+        int best_state = 0;
+        int[] results = new int[2] { 0, 0 };
 
         for (int i = 0; i < falls.length; i++)
-            for (int j = 0; i < falls[i].length; j++)
+            for (int j = 0; j < falls[i].length; j++)
             {
-                grid_buffer.copy_grid(grid);
+                GridIA grid_buffer = new GridIA(grid);
                 Point[] points = falls[i][j];
                 grid_buffer.put(points);
                 int score = grid_buffer.check_and_delete(points);
                 int evaluation = grid_buffer.eval();
+                int current_state = eval_state(score, evaluation);
+                if (current_state > best_state)
+                {
+                    results[0] = i;
+                    results[1] = j;
+                }
             }
+
+        return results;
+    }
+
+    public static int eval_state(int score, int evaluation)
+    {
+        return 0;
     }
 }
